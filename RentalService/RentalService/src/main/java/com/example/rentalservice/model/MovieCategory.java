a261673b86fac16c8a5f7ab1d2a48d66ea084aab
seewruuk <55555484+seewruuk@users.noreply.github.com>
2023-06-09 19:22:11 +0200

    Delete RentalService/RentalService directory

--- RentalService/RentalService/src/main/java/com/example/rentalservice/model/MovieCategory.java
+++
@@ -1,5 +0,0 @@
-package com.example.rentalservice.model;
-
-public enum MovieCategory {
-    ACTION, COMEDY, DRAMA, HORROR, ROMANCE, THRILLER, ADVENTURE, FANTASY, DOCUMENTARY, CRIME, SCIENCEFICTION;
-}
