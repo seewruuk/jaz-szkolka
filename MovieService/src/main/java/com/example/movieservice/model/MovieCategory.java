b37389dac84830cf86688f28ba26bf87ae2bbaad
seewruuk <55555484+seewruuk@users.noreply.github.com>
2023-06-09 19:22:00 +0200

    Delete MovieService directory

--- MovieService/src/main/java/com/example/movieservice/model/MovieCategory.java
+++
@@ -1,8 +0,0 @@
-package com.example.movieservice.model;
-
-public enum MovieCategory {
-
-    ACTION, COMEDY, DRAMA, HORROR, ROMANCE, THRILLER, ADVENTURE, FANTASY, DOCUMENTARY, CRIME, SCIENCEFICTION;
-
-
-}
